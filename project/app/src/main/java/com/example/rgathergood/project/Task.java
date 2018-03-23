8527a58c8c67b47cc06ccda6111ead1ecb428385
rgathergood <rgathergood@gmail.com>
2018-03-23 18:58:13 +0000

    Task class created, basic TDD

---
+++ project/app/src/main/java/com/example/rgathergood/project/Task.java
@@ -0,0 +1,42 @@
+package com.example.rgathergood.project;
+
+/**
+ * Created by rgathergood on 23/03/2018.
+ */
+
+public class Task {
+    int id;
+    String name;
+    String description;
+    String dateAdded;
+    boolean completed;
+
+    public Task(int id, String name, String description, String dateAdded, boolean completed) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.dateAdded = dateAdded;
+        this.completed = completed;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDateAdded() {
+        return dateAdded;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+}
---
+++ project/app/src/test/java/com/example/rgathergood/project/TestTask.java
@@ -0,0 +1,45 @@
+package com.example.rgathergood.project;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by rgathergood on 23/03/2018.
+ */
+
+public class TestTask {
+
+    Task task;
+
+    @Before
+    public void before() {
+        task = new Task(1, "Wash the Cat", "Need to buy more shampoo first", "23/03/17", false);
+    }
+
+    @Test
+    public void taskHasId() {
+        assertEquals(1, task.getId());
+    }
+
+    @Test
+    public void taskHasName() {
+        assertEquals("Wash the Cat", task.getName());
+    }
+
+    @Test
+    public void taskHasDescription() {
+        assertEquals("Need to buy more shampoo first", task.getDescription());
+    }
+
+    @Test
+    public void taskHasDateAdded() {
+        assertEquals("23/03/17", task.getDateAdded());
+    }
+
+    @Test
+    public void taskIsCompleted() {
+        assertEquals(false, task.isCompleted());
+    }
+}
