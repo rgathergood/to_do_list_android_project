85ebabbfa439a8ab86ae2277b3b66dcdc9f8ac86
rgathergood <rgathergood@gmail.com>
2018-03-23 22:14:26 +0000

    Basic XML created for MVP

--- project/app/src/main/java/com/example/rgathergood/project/MainActivity.java
+++ project/app/src/main/java/com/example/rgathergood/project/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.rgathergood.project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<Task> taskList;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //mDatabase
+
+        taskList = new ArrayList<>();
+        listView = (ListView) findViewById(R.id.listViewTasks);
     }
 }
